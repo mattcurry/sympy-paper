bb0a12a0f2c5c3639c38896332bfa06933918779
Aaron Meurer <asmeurer@gmail.com>
2016-10-05 14:20:24 -0400

    Allow authors/list_latex.py to be run from any directory

--- authors/list_latex.py
+++ authors/list_latex.py
@@ -2,13 +2,17 @@
 
 from __future__ import print_function
 
+
+from os.path import join, dirname, pardir
 from json import load
 from io import open
 
-author_list = load(open("authors.json"))
+authors_json = join(dirname(__file__), "authors.json")
+authors_tex = join(dirname(__file__), pardir, "authors.tex")
+author_list = load(open(authors_json))
 author_list = list(filter(lambda x: x["sympy_commits"] > 0, author_list))
 
-with open("../authors.tex", "w", encoding='utf-8') as f:
+with open(authors_tex, "w", encoding='utf-8') as f:
     for n, author in enumerate(author_list):
         f.write((u"\\author[%d]{%s}%%\n" % (n+1, author["name"])))
     for n, author in enumerate(author_list):
